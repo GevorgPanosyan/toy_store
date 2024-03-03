2b392b5657b88d64f1af173e4e0661e21e21a517
Gevorg <117856136+GevorgPanosyan@users.noreply.github.com>
2024-03-03 21:07:04 +0300

    Update toy_store.java
    Initialize the queue with Toy objects

--- toy_store.java
+++ toy_store.java
@@ -1,2 +1,19 @@
 import java.io.FileWriter;
-import
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+public class ToyStore{
+  private static final int TOTAL_GET_CALLS = 10;
+  private PriorityQueue<Toy>queue;
+
+  public static void main(String[] args){
+    ToyStore toyStore = new ToyStore();
+    toyStore.initializeQueue();
+    toyStore.executeGetCalls();
+    toyStore.writeResults To File();
+  }
+
+  //Initialize the queue with Toy objects
+  private void initializeQueue(){
+    queue = new PriorityQueue<>();
+    
